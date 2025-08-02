3fdcc3026bb032aae6d7a5172f8f82b56acbfb19
Konstantin Nered <konstantin.n616@gmail.com>
2025-08-03 00:12:32 +0700

    added route browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-export default function App(){
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Posts from './pages/Posts';
+import { useSelector } from 'react-redux';
+import { type RootState } from './store/store';
+import type { JSX } from 'react';
+
+function PrivateRoute({ children }: { children: JSX.Element }) {
+    const isAuth = useSelector((s: RootState) => s.auth.isAuth);
+    return isAuth ? children : <Navigate to='/login' />;
+}
+
+export default function App() {
     return (
-        <div>
-            app
-        </div>
-    )
+        <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route
+                path='/posts'
+                element={
+                    <PrivateRoute>
+                        <Posts />
+                    </PrivateRoute>
+                }
+            />
+            <Route path='*' element={<Navigate to='/posts' />} />
+        </Routes>
+    );
 }
